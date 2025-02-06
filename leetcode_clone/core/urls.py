594fa206d525ae825a8711a09b434387c18c10f4
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2025-02-06 20:34:22 +0530

    url changed

--- leetcode_clone/core/urls.py
+++ leetcode_clone/core/urls.py
@@ -6,5 +6,5 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.problem_list, name='problem_list'),
-    path('problems/<int:problem_id>/', views.problem_detail, name='problem_detail'),
+    path('problem_detail/<int:problem_id>/', views.problem_detail, name='problem_detail'),
 ]
