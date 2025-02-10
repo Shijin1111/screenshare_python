0fab3b885cfc40aaaa6d5da10c9e05e4b0fb65a9
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2025-02-10 15:28:14 +0530

    problem model changed

--- exampro/problems/models.py
+++ exampro/problems/models.py
@@ -3,8 +3,7 @@ from django.db import models
 class Problem(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    sample_input = models.TextField()
-    sample_output = models.TextField()
+    sample_testcases = models.TextField()
 
 class TestCase(models.Model):
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE, related_name='test_cases')
